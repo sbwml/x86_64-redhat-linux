6c206178ae04a54b7b290897eaecc972615c379a
sbwml <984419930@qq.com>
2020-09-09 12:39:39 +0800

    gcc-10.2.0
    yum install gcc gcc-c++ zlib-devel bzip2 texinfo bison byacc yacc flex expect
    
    ../configure \
      --prefix=/opt/usr/local/gcc \
      --mandir=/opt/usr/local/gcc/share/man \
      --infodir=/opt/usr/local/gcc/share/info \
      --with-bugurl=http://bugzilla.redhat.com/bugzilla \
      --enable-bootstrap \
      --enable-shared \
      --enable-threads=posix \
      --enable-checking=release \
      --with-system-zlib \
      --enable-__cxa_atexit \
      --disable-libunwind-exceptions \
      --enable-gnu-unique-object \
      --enable-linker-build-id \
      --with-linker-hash-style=gnu \
      --enable-languages=c,c++,objc,obj-c++,fortran,lto \
      --enable-plugin \
      --enable-initfini-array \
      --disable-libgcj \
      --enable-gnu-indirect-function \
      --with-tune=generic \
      --with-arch_32=x86-64 \
      --build=x86_64-redhat-linux \
      --disable-multilib

---
+++ lib/gcc/x86_64-redhat-linux/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/opt/usr/local/gcc --mandir=/opt/usr/local/gcc/share/man --infodir=/opt/usr/local/gcc/share/info --with-bugurl=http://bugzilla.redhat.com/bugzilla --enable-bootstrap --enable-shared --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-libunwind-exceptions --enable-gnu-unique-object --enable-linker-build-id --with-linker-hash-style=gnu --enable-languages=c,c++,objc,obj-c++,fortran,lto --enable-plugin --enable-initfini-array --disable-libgcj --enable-gnu-indirect-function --with-tune=generic --with-arch_32=x86-64 --build=x86_64-redhat-linux --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch_32", "x86-64" }, { "arch_64", "x86-64" }, { "tune", "generic" } };
